18257533b43681e10f62977392d144d16d807f4f
Caellian <tin.svagelj.email@gmail.com>
2016-07-31 17:37:15 +0200

    Version 1.1.0
    Created implementation of Matrix and Vector for other number types.
    Moved type related objects to parent .scala file.
    
    Signed-off-by: Caellian <tin.svagelj.email@gmail.com>

--- src/main/scala/hr/caellian/math/geometry/MatrixF.scala
+++ src/main/scala/hr/caellian/math/geometry/MatrixF.scala
@@ -1,19 +1,25 @@
 /*
- * CaellianMath, math library
- * Copyright (C) 2016 Caellian
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Tin Švagelj <tin.svagelj.email@gmail.com> a.k.a. Caellian
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  */
 
 package hr.caellian.math.geometry
@@ -21,7 +27,7 @@ package hr.caellian.math.geometry
 import java.nio._
 import java.util
 
-import hr.caellian.math.util.DataUtil
+import hr.caellian.math.util.Data
 import org.apache.commons.math3.linear._
 import org.apache.commons.math3.util.FastMath
 
@@ -302,7 +308,7 @@ class MatrixF(var matrix: Array[Array[Float]]) extends Matrix[Float] {
 	  * @return hashcode of this matrix.
 	  */
 	override def hashCode: Int = {
-		DataUtil.hashCode(matrix)
+		Data.hashCode(matrix)
 	}
 
 	/**
@@ -317,7 +323,7 @@ class MatrixF(var matrix: Array[Array[Float]]) extends Matrix[Float] {
 		if (!o.isInstanceOf[VectorF]) {
 			return false
 		}
-		DataUtil.equals(matrix, o.asInstanceOf[MatrixF].matrix)
+		Data.equals(matrix, o.asInstanceOf[MatrixF].matrix)
 	}
 
 	/**
@@ -355,7 +361,7 @@ class MatrixF(var matrix: Array[Array[Float]]) extends Matrix[Float] {
 }
 
 /**
-  * Object used to create {@link hr.caellian.math.geometry.MatrixF MatrixF} matrices and ensure their consistency.
+  * Object used to create {@link MatrixF} matrices and ensure their consistency.
   * Use initializer functions from this object only if there is no alternative ones in more specific objects as some
   * of these are very CPU intensive.
   *
@@ -531,4 +537,410 @@ object MatrixF {
 			}
 		}))
 	}
+}
+
+/**
+	* Utility object containing initializers for basic 2x2 matrices.
+	* These functions should be used instead of any provided by {@link MatrixF}
+	* wherever possible as they are supposed to perform faster.
+	*
+	* @author Caellian
+	*/
+object Matrix2F {
+	/**
+		* Initializes rotation matrix using degrees.
+		*
+		* @param degrees Degrees to rotate objects multiplied by this matrix in positive direction.
+		*
+		* @return rotation matrix
+		*/
+	def initRotation(degrees: Float): MatrixF = {
+		if (degrees == 0) {
+			return initIdentityMatrix()
+		}
+
+		val result = Array.ofDim[Float](2, 2)
+		result(0)(0) = FastMath.cos(FastMath.toRadians(degrees)).toFloat
+		result(0)(1) = FastMath.sin(FastMath.toRadians(degrees)).toFloat
+		result(1)(0) = -FastMath.sin(FastMath.toRadians(degrees)).toFloat
+		result(1)(1) = FastMath.cos(FastMath.toRadians(degrees)).toFloat
+
+		new MatrixF(result)
+	}
+
+	/**
+		* Initializes a new 2x2 identity matrix.
+		*
+		* @return identity matrix.
+		*/
+	def initIdentityMatrix(): MatrixF = {
+		new MatrixF(Array.tabulate[Float](2, 2)((x, y) => {
+			if (x == y) {
+				1
+			} else {
+				0
+			}
+		}))
+	}
+
+	/**
+		* Initializes a new 2x2 scaling matrix.
+		*
+		* @param scale scale.
+		*
+		* @return scale matrix.
+		*/
+	def initScalingMatrix(scale: Array[Float]): MatrixF = {
+		assert(scale.length == 2, "Translation must have 2 values!")
+		new MatrixF(Array.tabulate[Float](2, 2)((x, y) => {
+			if (x == y) {
+				scale(x)
+			} else {
+				0
+			}
+		}))
+	}
+}
+
+/**
+  * Utility object containing initializers for basic 3x3 matrices.
+  * These functions should be used instead of any provided by {@link MatrixF}
+  * wherever possible as they are supposed to perform faster.
+  *
+  * @author Caellian
+  */
+object Matrix3F {
+  /**
+    * Initializes rotation matrix using forward, up and right vector.
+    *
+    * @param forward forward vector
+    * @param up      up vector
+    * @param right   right vector
+    *
+    * @return rotation matrix
+    */
+  def initRotationMatrix(forward: VectorF, up: VectorF, right: VectorF): MatrixF = {
+    new MatrixF(Array(right.asArray, up.asArray, forward.asArray))
+  }
+
+  /**
+    * Turns this matrix into a rotation matrix.
+    *
+    * @param a     first argument.
+    * @param b     second argument.
+    * @param angle degrees to rotate in objects multiplied by this rotation matrix.
+    *
+    * @return plane rotation matrix.
+    */
+  def initPlaneRotation(a: Int, b: Int, angle: Float): MatrixF = {
+    val ai = a - 1
+    val bi = b - 1
+    val matrix = Array.ofDim[Float](4,4)
+    for (x <- matrix.indices) {
+      for (y <- matrix(0).indices) {
+        val value = (y, x) match {
+          case (`ai`, `ai`) => FastMath.cos(FastMath.toRadians(angle)).toFloat
+          case (`bi`, `bi`) => FastMath.cos(FastMath.toRadians(angle)).toFloat
+          case (`ai`, `bi`) => -FastMath.sin(FastMath.toRadians(angle)).toFloat
+          case (`bi`, `ai`) => FastMath.sin(FastMath.toRadians(angle)).toFloat
+          case _ => {
+            if (x == y) {
+              1
+            } else {
+              0
+            }
+          }
+        }
+        matrix(y)(x) = value
+      }
+    }
+    MatrixF(matrix)
+  }
+
+  /**
+    * Initializes rotation matrix using degree rotation in x, y & z direction.
+    *
+    * @param x degree rotation in x direction
+    * @param y degree rotation in y direction
+    * @param z degree rotation in z direction
+    *
+    * @return rotation matrix
+    */
+  def initRotationMatrix(x: Float, y: Float, z: Float): MatrixF = {
+    val rx = Matrix3F.initPlaneRotation(2, 3, x)
+    val ry = Matrix3F.initPlaneRotation(3, 1, y)
+    val rz = Matrix3F.initPlaneRotation(1, 2, z)
+    MatrixF((rz * ry * rx).asArray)
+  }
+
+  /**
+    * Initializes a new 3x3 scaling matrix.
+    *
+    * @param scale scale.
+    *
+    * @return scale matrix.
+    */
+  def initScalingMatrix(scale: Array[Float]): MatrixF = {
+    assert(scale.length == 3, "Translation must have 3 values!")
+    new MatrixF(Array.tabulate[Float](3, 3)((x, y) => {
+      if (x == y) {
+        scale(x)
+      } else {
+        0
+      }
+    }))
+  }
+
+  /**
+    * Initializes a new 3x3 translation matrix.
+    *
+    * @param location relative location.
+    *
+    * @return translation matrix.
+    */
+  def initTranslationMatrix(location: Array[Float]): MatrixF = {
+    assert(location.length == 2, "Translation must have 2 coordinates!")
+    new MatrixF(Array.tabulate[Float](3, 3)((x,
+                                             y) => {
+      if (x == y) {
+        1
+      } else if (y == location.length && x < location.length) {
+        location(x)
+      } else {
+        0
+      }
+    }))
+  }
+
+  /**
+    * Initializes a new 3x3 identity matrix.
+    *
+    * @return identity matrix.
+    */
+  def initIdentityMatrix(): MatrixF = {
+    new MatrixF(Array.tabulate[Float](3, 3)((x, y) => {
+      if (x == y) {
+        1
+      } else {
+        0
+      }
+    }))
+  }
+}
+
+/**
+  * Utility object containing initializers for basic 4x4 matrices.
+  * These functions should be used instead of any provided by {@link MatrixF}
+  * wherever possible as they are supposed to perform faster.
+  *
+  * @author Caellian
+  */
+object Matrix4F {
+  /**
+    * Initializes perspective transformation matrix.
+    *
+    * @param fov         field of view.
+    * @param aspectRatio aspect ration.
+    * @param clipNear    front clipping position.
+    * @param clipFar     back clipping position.
+    *
+    * @return perspective transformation matrix.
+    */
+  def initPerspectiveMatrix(fov: Float, aspectRatio: Float, clipNear: Float, clipFar: Float): MatrixF = {
+    val result = Array.ofDim[Float](4, 4)
+    val fowAngle: Float = FastMath.tan(fov / 2).toFloat
+    val clipRange: Float = clipNear - clipFar
+    result(0)(0) = 1.0f / (fowAngle * aspectRatio)
+    result(0)(1) = 0
+    result(0)(2) = 0
+    result(0)(3) = 0
+    result(1)(0) = 0
+    result(1)(1) = 1.0f / fowAngle
+    result(1)(2) = 0
+    result(1)(3) = 0
+    result(2)(0) = 0
+    result(2)(1) = 0
+    result(2)(2) = (-clipNear - clipFar) / clipRange
+    result(2)(3) = 2 * clipFar * clipNear / clipRange
+    result(3)(0) = 0
+    result(3)(1) = 0
+    result(3)(2) = 1
+    result(3)(3) = 0
+    new MatrixF(result)
+  }
+
+  /**
+    * Initializes orthographic transformation matrix.
+    *
+    * @param left     left clipping position.
+    * @param right    right clipping position.
+    * @param bottom   bottom clipping position.
+    * @param top      top clipping position.
+    * @param clipNear front clipping position.
+    * @param clipFar  back clipping position.
+    *
+    * @return orthographic transformation matrix
+    */
+  def initOrthographicMatrix(left: Float,
+                             right: Float,
+                             bottom: Float,
+                             top: Float,
+                             clipNear: Float,
+                             clipFar: Float): MatrixF = {
+    val result = Array.ofDim[Float](4, 4)
+    val width: Float = right - left
+    val height: Float = top - bottom
+    val depth: Float = clipFar - clipNear
+    result(0)(0) = 2 / width
+    result(0)(1) = 0
+    result(0)(2) = 0
+    result(0)(3) = -(right + left) / width
+    result(1)(0) = 0
+    result(1)(1) = 2 / height
+    result(1)(2) = 0
+    result(1)(3) = -(top + bottom) / height
+    result(2)(0) = 0
+    result(2)(1) = 0
+    result(2)(2) = -2 / depth
+    result(2)(3) = -(clipFar + clipNear) / depth
+    result(3)(0) = 0
+    result(3)(1) = 0
+    result(3)(2) = 0
+    result(3)(3) = 1
+    new MatrixF(result)
+  }
+
+  /**
+    * Initializes rotation matrix using forward and up vector by calculating
+    * right vector.
+    *
+    * @param forward forward 3f vector.
+    * @param up      up 3f vector.
+    *
+    * @return rotation matrix.
+    */
+  def initRotationMatrix(forward: VectorF, up: VectorF): MatrixF = {
+    val f = forward.normalized
+    val r = up.normalized.cross(f)
+    val u = f.cross(r)
+    Matrix4F.initRotationMatrix(f, u, r)
+  }
+
+  /**
+    * Initializes rotation matrix using forward, up and right vector.
+    *
+    * @param forward forward 3f vector.
+    * @param up      up 3f vector.
+    * @param right   right 3f vector.
+    *
+    * @return rotation matrix.
+    */
+  def initRotationMatrix(forward: VectorF, up: VectorF, right: VectorF): MatrixF = {
+    val result = Array.ofDim[Float](4, 4)
+    result(0)(0) = right.data(X)
+    result(0)(1) = right.data(Y)
+    result(0)(2) = right.data(Z)
+    result(0)(3) = 0
+    result(1)(0) = up.data(X)
+    result(1)(1) = up.data(Y)
+    result(1)(2) = up.data(Y)
+    result(1)(3) = 0
+    result(2)(0) = forward.data(X)
+    result(2)(1) = forward.data(Y)
+    result(2)(2) = forward.data(Z)
+    result(2)(3) = 0
+    result(3)(0) = 0
+    result(3)(1) = 0
+    result(3)(2) = 0
+    result(3)(3) = 1
+    new MatrixF(result)
+  }
+
+  /**
+    * Turns this matrix into a rotation matrix.
+    *
+    * @param a     first argument.
+    * @param b     second argument.
+    * @param angle degrees to rotate in objects multiplied by this rotation matrix.
+    *
+    * @return plane rotation matrix.
+    */
+  def initPlaneRotation(a: Int, b: Int, angle: Float): MatrixF = {
+    val ai = a - 1
+    val bi = b - 1
+    val matrix = Array.ofDim[Float](4,4)
+    for (x <- matrix.indices) {
+      for (y <- matrix(0).indices) {
+        val value = (y, x) match {
+          case (`ai`, `ai`) => FastMath.cos(FastMath.toRadians(angle)).toFloat
+          case (`bi`, `bi`) => FastMath.cos(FastMath.toRadians(angle)).toFloat
+          case (`ai`, `bi`) => -FastMath.sin(FastMath.toRadians(angle)).toFloat
+          case (`bi`, `ai`) => FastMath.sin(FastMath.toRadians(angle)).toFloat
+          case _ => {
+            if (x == y) {
+              1
+            } else {
+              0
+            }
+          }
+        }
+        matrix(y)(x) = value
+      }
+    }
+    MatrixF(matrix)
+  }
+
+  /**
+    * Initializes a new 4x4 scaling matrix.
+    *
+    * @param scale scale.
+    *
+    * @return scale matrix.
+    */
+  def initScalingMatrix(scale: Array[Float]): MatrixF = {
+    assert(scale.length == 4, "Translation must have 4 values!")
+    new MatrixF(Array.tabulate[Float](4, 4)((x, y) => {
+      if (x == y) {
+        scale(x)
+      } else {
+        0
+      }
+    }))
+  }
+
+  /**
+    * Initializes a new 4x4 translation matrix.
+    *
+    * @param location relative location.
+    *
+    * @return translation matrix.
+    */
+  def initTranslationMatrix(location: Array[Float]): MatrixF = {
+    assert(location.length == 3, "Translation must have 3 coordinates!")
+    new MatrixF(Array.tabulate[Float](4, 4)((x,
+                                             y) => {
+      if (x == y) {
+        1
+      } else if (y == location.length && x < location.length) {
+        location(x)
+      } else {
+        0
+      }
+    }))
+  }
+
+  /**
+    * Initializes a new 4x4 identity matrix.
+    *
+    * @return identity matrix.
+    */
+  def initIdentityMatrix(): MatrixF = {
+    new MatrixF(Array.tabulate[Float](4, 4)((x, y) => {
+      if (x == y) {
+        1
+      } else {
+        0
+      }
+    }))
+  }
 }
--- src/main/scala/hr/caellian/math/geometry/VectorF.scala
+++ src/main/scala/hr/caellian/math/geometry/VectorF.scala
@@ -1,26 +1,32 @@
 /*
- * CaellianMath, math library
- * Copyright (C) 2016 Caellian
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Tin Švagelj <tin.svagelj.email@gmail.com> a.k.a. Caellian
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  */
 
 package hr.caellian.math.geometry
 
 import java.nio._
 
-import hr.caellian.math.util.DataUtil
+import hr.caellian.math.util.Data
 import org.apache.commons.math3.util.FastMath
 
 /**
@@ -284,7 +290,7 @@ class VectorF(val data: Array[Float]) extends Vector[Float] {
 	  * @return hashcode of this vector.
 	  */
 	override def hashCode: Int = {
-		DataUtil.hashCode(data)
+		Data.hashCode(data)
 	}
 
 	/**
@@ -305,7 +311,7 @@ class VectorF(val data: Array[Float]) extends Vector[Float] {
 }
 
 /**
-  * Object used to create {@link hr.caellian.math.geometry.VectorF VectorF} vectors and ensure their consistency.
+  * Object used to create {@link VectorF} vectors and ensure their consistency.
   * Use initializer functions from this object only if there is no alternative ones in more specific objects as some
   * of these are very CPU intensive.
   *
@@ -345,5 +351,61 @@ object VectorF {
 	  */
 	def apply(buffer: FloatBuffer): VectorF = {
 		new VectorF((0 until buffer.capacity()).map(buffer.get).toArray)
+	}
+}
+
+/**
+	* Object containing utility functions for quaternion initialization.
+	*
+	* @author Caellian
+	*/
+object QuaternionF {
+	/**
+		* Creates a new quaternion (4D vector) using {@link VectorF} class from
+		* given rotation matrix.
+		*
+		* @param rotationMatrix rotation matrix to use for quaternion creation.
+		* @return resulting quaternion.
+		*/
+	def fromRotationMatrix(rotationMatrix: MatrixF): VectorF = {
+		assert(rotationMatrix.matrix.length == 4 && rotationMatrix.matrix.forall(_.length == 4), "")
+		var x = 0f
+		var y = 0f
+		var z = 0f
+		var w = 0f
+		val trace: Float = rotationMatrix.get(0, 0) + rotationMatrix.get(1, 1) + rotationMatrix.get(2, 2)
+
+		if (trace > 0) {
+			val s: Float = 0.5f / FastMath.sqrt(trace + 1.0f).toFloat
+			x = 0.25f / s
+			y = (rotationMatrix.get(1, 2) - rotationMatrix.get(2, 1)) * s
+			z = (rotationMatrix.get(2, 0) - rotationMatrix.get(0, 2)) * s
+			w = (rotationMatrix.get(0, 1) - rotationMatrix.get(1, 0)) * s
+		}
+		else {
+			if (rotationMatrix.get(0, 0) > rotationMatrix.get(1, 1) && rotationMatrix.get(0, 0) > rotationMatrix.get(2, 2)) {
+				val s: Float = 2.0f * FastMath.sqrt(1.0f + rotationMatrix.get(0, 0) - rotationMatrix.get(1, 1) - rotationMatrix.get(2, 2)).toFloat
+				x = (rotationMatrix.get(1, 2) - rotationMatrix.get(2, 1)) / s
+				y = 0.25f * s
+				z = (rotationMatrix.get(1, 0) + rotationMatrix.get(0, 1)) / s
+				w = (rotationMatrix.get(2, 0) + rotationMatrix.get(0, 2)) / s
+			}
+			else if (rotationMatrix.get(1, 1) > rotationMatrix.get(2, 2)) {
+				val s: Float = 2.0f * FastMath.sqrt(1.0f + rotationMatrix.get(1, 1) - rotationMatrix.get(0, 0) - rotationMatrix.get(2, 2)).toFloat
+				x = (rotationMatrix.get(2, 0) - rotationMatrix.get(0, 2)) / s
+				y = (rotationMatrix.get(1, 0) + rotationMatrix.get(0, 1)) / s
+				z = 0.25f * s
+				w = (rotationMatrix.get(2, 1) + rotationMatrix.get(1, 2)) / s
+			}
+			else {
+				val s: Float = 2.0f * FastMath.sqrt(1.0f + rotationMatrix.get(2, 2) - rotationMatrix.get(0, 0) - rotationMatrix.get(1, 1)).toFloat
+				x = (rotationMatrix.get(0, 1) - rotationMatrix.get(1, 0)) / s
+				y = (rotationMatrix.get(2, 0) + rotationMatrix.get(0, 2)) / s
+				z = (rotationMatrix.get(1, 2) + rotationMatrix.get(2, 1)) / s
+				w = 0.25f * s
+			}
+		}
+
+		VectorF(x, y, z, w).normalized
 	}
 }
